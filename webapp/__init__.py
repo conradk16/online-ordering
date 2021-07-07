30c443a074f1162480f0055f9111aa3524aa1fb2
Stefan Kuklinsky <sckuklinsky@berkeley.edu>
2021-07-06 17:25:42 -0700

    finish sign up and log in logic, make bare signup-select-plan page

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -22,7 +22,7 @@ from .models.user import User
 db.create_all()
 db.session.commit()
 if not User.query.filter_by(email_address="kuklinskywork@gmail.com").first():
-    admin_user = User(email_address="kuklinskywork@gmail.com", password=bcrypt.generate_password_hash("36e&'&4K`c4mp~#cjZZ.6q@!#3?APZ%*").decode('utf-8'))
+    admin_user = User(email_address="kuklinskywork@gmail.com", password=bcrypt.generate_password_hash("36e&'&4K`c4mp~#cjZZ.6q@!#3?APZ%*").decode('utf-8'), stripe_customer_id="kuklinskywork@gmail.com")
     admin_user.add_to_db()
     db.session.commit()
 
--- webapp/views/home.py
+++ webapp/views/home.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, request, render_template, redirect, jsonify, session
+from flask import Blueprint, request, render_template, redirect, jsonify, session, url_for
+from flask_login import login_user, logout_user, current_user
+
 from webapp.models.user import User
 from webapp import db, bcrypt
+
 import stripe
-from flask_login import login_user, logout_user, current_user
 
 home = Blueprint('home', __name__)
 
@@ -11,119 +13,120 @@ home = Blueprint('home', __name__)
 def homepage():
     return render_template('homepage.html')
 
-# signup endpoint - signup page and requests to sign up
-@home.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'GET':
-        if current_user.is_authenticated:
-            return redirect('/account')
-        else:
-            return render_template('signup.html', user_already_exists=False)
-    elif request.method == 'POST':
-        user = User.query.filter_by(email_address=request.form['email_address']).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, request.form['password']):
-                if user.stripe_customer_id:
-                    return redirect('/account')
-                else:
-                    return redirect('/signup/select-plan')
-            else:
-                return render_template('signup.html', user_already_exists=True)
-        else:
-            user = User(email_address=request.form['email_address'], password=request.form['password'])
-            user.add_to_db()
-            return redirect('/signup/select-plan')
-
-
-@home.route('/signup/submit-email', methods=['POST'])
-def signup_submit_email():
-    session['email_address'] = request.form['email_address']
-
-    user = User.query.filter_by(email_address=request.form['email_address']).first()
-    if user:
-        if user.password:
-            return redirect('/signup/enter-password')
-        else:
-            return redirect('/signup/create-password')
-    else:
-        user = User(email_address=request.form['email_address'])
-        user.add_to_db()
-        return redirect('/signup/create-password')
-
-@home.route('/signup/enter-password', methods=['GET', 'POST'])
-def signup_enter_password():
-    return render_template('signup-enter-password.html', email_address=session['email_address'])
-
-@home.route('/signup/create-password')
-def signup_create_password():
-    return render_template('signup-create-password.html', email_address=session['email_address'])
-
-@home.route('/signup/submit-email-password', methods=['POST'])
-def signup_submit_email_password():
-    user = User.query.filter_by(email_address=request.form['email_address']).first()
-    if user.password:
-        if bcrypt.check_password_hash(user.password, request.form['password']):
-            if user.stripe_customer_id:
-                return redirect('/account')
-            else:
-                return redirect('/signup/select-plan')
-        else:
-            return redirect('/signup/enter-password') # incorrect password
-    else:
-        hashed_password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
-        user.password = hashed_password
-        db.session.commit()
-        return redirect('/signup/select-plan')
-
-@home.route('/signup/select-plan')
-def signup_select_plan():
-    return render_template('signup-select-plan.html')
-
-
-# POST endpoint for businesses to pay us - requires an HTML form and redirects them to Stripe's checkout URL
-@home.route('/signup/payment', methods=['POST'])
-def signup_payment():
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            success_url='https://m3orders.com/login',
-            cancel_url='https://m3orders.com',
-            customer_email=session['email_address'],
-            client_reference_id=session['email_address'],
-            payment_method_types=['card'],
-            mode='subscription',
-            line_items=[{
-                'price': request.form['price_id'],
-                'quantity': 1
-            }],
-        )
-        return redirect(checkout_session.url, code=303);
-    except Exception as e:
-        return jsonify({'error': {'message': str(e)}}), 400
-
-
 # login page, also a POST endpoint for attempting to log in
 @home.route('/login', methods=['GET', 'POST'])
 def login_page():
     if request.method == 'GET':
         if current_user.is_authenticated:
             return redirect('/account')
-        return render_template('login-page.html')
+        else:
+            if 'invalid_credentials' in session:
+                invalid_credentials = session['invalid_credentials']
+            else:
+                invalid_credentials = "false"
+            session['invalid_credentials'] = "false"
+            return render_template('login.html', invalid_credentials=invalid_credentials)
     elif request.method == 'POST':
+        if not is_valid_login_post_request(request):
+            session['invalid_credentials'] = "true"
+            return redirect(url_for('home.login_page'))
+        
         user = User.query.filter_by(email_address=request.form['email_address']).first()
         if user and user.password:
             if bcrypt.check_password_hash(user.password, request.form['password']):
-                if user.stripe_customer_id or user.email_address == "kuklinskywork@gmail.com":
-                    login_user(user, remember=True)
+                login_user(user, remember=request.form['remember_me'])
+                if user.stripe_customer_id:
                     return redirect('/account')
                 else:
                     return redirect('/signup/select-plan')
             else:
-                return redirect('/login')
+                session['invalid_credentials'] = "true"
+                return redirect(url_for('home.login_page'))
         else:
-            return redirect('/login')
+            session['invalid_credentials'] = "true"
+            return redirect(url_for('home.login_page'))
 
-# logout user
+# logout POST endpoint
 @home.route('/logout')
 def logout():
     logout_user()
     return redirect('/')
+
+# signup page, also a POST endpoint for signing up
+@home.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect('/account')
+        else:
+            return render_template('signup.html', user_already_exists="false")
+    elif request.method == 'POST':
+        if not is_valid_signup_post_request(request):
+            return redirect('/signup')
+
+        user = User.query.filter_by(email_address=request.form['email_address']).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, request.form['password']):
+                login_user(user, remember=False)
+                if user.stripe_customer_id:
+                    return redirect('/account')
+                else:
+                    return redirect('/signup/select-plan')
+            else:
+                return render_template('signup.html', user_already_exists="true")
+        else:
+            user = User(email_address=request.form['email_address'], password=bcrypt.generate_password_hash(request.form['password']).decode('utf-8'))
+            user.add_to_db()
+            login_user(user, remember=False)
+            return redirect('/signup/select-plan')
+
+# select plan page, also a POST endpoint for choosing a plan and being redirected to Stripe checkout
+@home.route('/signup/select-plan', methods=['GET', 'POST'])
+def signup_select_plan():
+    if request.method == 'GET':
+        if not current_user.is_authenticated:
+            return redirect(url_for('home.login_page'))
+        else:
+            return render_template('signup-select-plan.html')
+    elif request.method == 'POST':
+        if (not current_user.is_authenticated) or (not is_valid_signup_select_plan_post_request(request)):
+            return redirect(url_for('home.login_page'))
+        else:
+            try:
+                checkout_session = stripe.checkout.Session.create(
+                success_url='https://m3orders.com/login',
+                cancel_url='https://m3orders.com',
+                customer_email=current_user.email_address,
+                client_reference_id=current_user.email_address,
+                payment_method_types=['card'],
+                mode='subscription',
+                line_items=[{
+                    'price': request.form['price_id'],
+                    'quantity': 1
+                }]
+            )
+                return redirect(checkout_session.url, code=303);
+            except Exception as e:
+                return jsonify({'error': {'message': str(e)}}), 400
+
+def is_valid_login_post_request(request):
+    if request.form:
+        if ('email_address' in request.form) and ('password' in request.form) and ('remember_me' in request.form):
+            if (len(request.form['email_address']) <= 100) and (len(request.form['email_address']) > 0) and (len(request.form['password']) <= 100) and (len(request.form['password']) > 0):
+                return True
+    return False
+
+def is_valid_signup_post_request(request):
+    if request.form:
+        if ('email_address' in request.form) and ('password' in request.form):
+            if (len(request.form['email_address']) <= 100) and (len(request.form['email_address']) > 0) and (len(request.form['password']) <= 100) and (len(request.form['password']) > 0):
+                return True
+    return False
+
+def is_valid_signup_select_plan_post_request(request):
+    if request.form:
+        if 'price_id' in request.form:
+            return True
+    return False
+
+
