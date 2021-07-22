43c7db739939379a6b6bca6b2d9c625708c6baf3
Stefan Kuklinsky <sckuklinsky@berkeley.edu>
2021-07-22 12:01:17 -0700

    working towards in_stock option

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -18,13 +18,13 @@ if use_test_webhook_with_live_m3_url:
     env['stripe_webhook_connect_signing_secret'] = 'whsec_ou3eKzCLgsvLSM8CuYgirpXhatVsArlE'
 
 if PROD:
-    env['stripe_secret_api_key'] = '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'
+    stripe.api_key = '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'
     env['stripe_webhook_account_signing_secret'] = 'whsec_5fk3ps0nQqpEHiSRFIVTXMfunysdzOTg'
     env['stripe_webhook_connect_signing_secret'] = 'whsec_w90BqL9nSa8at3kwalBAmi7UUEa37WPt'
     env['stripe_hardware_product_price_id'] = 'price_1JFrYYLGQW192ovfqc3fdxV9'
  
 else:
-    env['stripe_secret_api_key'] = 'sk_test_51J8elwLGQW192ovfZdXa5R8KnXuzvceiy9kCV7wojYHBG3L4Y0H0W4MjpXFTgZhUEw9Qzn1naBr5mR2MXUCnczOo00nsenWbzL'
+    stripe.api_key = 'sk_test_51J8elwLGQW192ovfZdXa5R8KnXuzvceiy9kCV7wojYHBG3L4Y0H0W4MjpXFTgZhUEw9Qzn1naBr5mR2MXUCnczOo00nsenWbzL'
     env['stripe_webhook_account_signing_secret'] = 'whsec_XDeJeqt7NpBy9HfWB5qmd4iO9dCrtmap'
     env['stripe_webhook_connect_signing_secret'] = 'whsec_eKndjsZQ3ShaeMdqo5wp13gziZLI7as5'
     env['stripe_hardware_product_price_id'] = 'price_1JFMNILGQW192ovflSPEK3sp'
@@ -75,4 +75,5 @@ app.register_blueprint(account)
 app.register_blueprint(order)
 app.register_blueprint(webhook)
 
-stripe.api_key = env['stripe_secret_api_key']
+# run webapp.static.menus.py to create the menus and add them to the database
+import webapp.static.menus
