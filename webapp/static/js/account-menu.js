271310ec3ca86c3889b6864edf0dd563bfe5b56d
Stefan Kuklinsky <sckuklinsky@berkeley.edu>
2021-07-23 17:01:00 -0700

    we messin around out here

--- webapp/static/js/account-menu.js
+++ webapp/static/js/account-menu.js
@@ -14,27 +14,19 @@ function run_on_resize() {
 function check_if_view_orders() {
     var orders_link = document.getElementById("account-menu-script").getAttribute('data-order_url');
 
-    //var orders_link = "None";
-
+    var menu_view_orders_block = document.getElementById("menu_view_orders_block");
     var menu_view_orders = document.getElementById("menu_view_orders");
     if (orders_link == "None") {
+        menu_view_orders_block.display = "block";
+        menu_view_orders_block.style.backgroundColor = "#fff";
+
         menu_view_orders.style.cursor = "default";
         menu_view_orders.style.color = "gray";
         menu_view_orders.style.pointerEvents = "none";
-
-        var menu_view_orders_block = document.getElementById("menu_view_orders_block");
-        menu_view_orders_block.display = "block";
-        menu_view_orders_block.style.backgroundColor = "#fff";
     } else {
         orders_link = "https://m3orders.com/" + orders_link;
         menu_view_orders.style.cursor = "pointer";
         menu_view_orders.style.color = "black";
         menu_view_orders.style.pointerEvents = "auto";
-        
     }
-}
-
-var perfEntries = performance.getEntriesByType("navigation");
-if (perfEntries[0].type === "back_forward") {
-    location.reload(true);
 }
