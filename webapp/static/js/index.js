b060f0d2f756cafa66440bc4901cd07c77d686ce
conradk16 <ckuklinsky21@amherst.edu>
2021-07-30 18:26:25 -0700

    updated online-ordering to include no-payment-processing

--- webapp/static/js/index.js
+++ webapp/static/js/index.js
@@ -5,6 +5,11 @@ var stripe_publishable_api_key = document.getElementById("helper").getAttribute(
 var stripe = Stripe(stripe_publishable_api_key, {
     stripeAccount: stripe_account_id
   });
+//var customers_pay_online = document.getElementById("helper").getAttribute('data-customers_pay_online') == "True";
+var customers_pay_online = false;
+//var restaurant_display_name = document.getElementById("helper").getAttribute('data-restaurant_display_name');
+var restaurant_display_name = "Demo Restaurant";
+
 
 function registerElements(elements, exampleName) {
   var formClass = '.' + exampleName;
@@ -14,6 +19,34 @@ function registerElements(elements, exampleName) {
   var resetButton = example.querySelector('a.reset');
   var error = form.querySelector('.error');
   var errorMessage = error.querySelector('.message');
+
+  
+  if (!customers_pay_online) {
+    // change title
+    var title = document.getElementsByClassName("title")[0];
+    title.innerHTML = "Confirm Order"
+  
+    // remove card input
+    var card_input = document.getElementById("example1-card");
+    var card_row = document.getElementById("card-row");
+    var card_fieldset = document.getElementById("card-fieldset");
+    card_input.remove();
+    card_row.remove();
+    card_fieldset.remove();
+
+    // set button text
+    var button = document.getElementById("submit-button");
+    button.innerHTML = "Place Pickup Order at " + restaurant_display_name;
+  } else {
+    // set button text
+    var price = document.getElementById("helper").getAttribute('data-price');
+    price = price / 100;
+    price = price.toFixed(2);
+    var submit_button = document.getElementById("submit-button");
+    submit_button.innerHTML = "Pay $" + price;
+  }
+
+  
 
   function enableInputs() {
     Array.prototype.forEach.call(
@@ -83,18 +116,20 @@ function registerElements(elements, exampleName) {
 
     // Trigger HTML5 validation UI on the form if any of the inputs fail
     // validation.
-    var plainInputsValid = true;
-    Array.prototype.forEach.call(form.querySelectorAll('input'), function(
-      input
-    ) {
-      if (input.checkValidity && !input.checkValidity()) {
-        plainInputsValid = false;
-        return;
-      }
-    });
-    if (!plainInputsValid) {
-      triggerBrowserValidation();
-      return;
+    if (customers_pay_online) {
+        var plainInputsValid = true;
+        Array.prototype.forEach.call(form.querySelectorAll('input'), function(
+          input
+        ) {
+          if (input.checkValidity && !input.checkValidity()) {
+            plainInputsValid = false;
+            return;
+          }
+        });
+        if (!plainInputsValid) {
+          triggerBrowserValidation();
+          return;
+        }
     }
 
     // Hide title
@@ -108,23 +143,55 @@ function registerElements(elements, exampleName) {
     disableInputs();
 
     // set order_info form data
-    var name_input = document.getElementById("name_inp");
+    
     var name_element = document.getElementById("example1-name");
-    name_input.value = name_element.value;
-
-    var email_input = document.getElementById("email_inp");
     var email_element = document.getElementById("example1-email");
+
+    var form = document.getElementById("order_info");
+    var name_input = document.createElement("input");
+    name_input.type = "hidden";
+    name_input.name = "customer_name";
+    name_input.value = name_element.value;
+    form.appendChild(name_input);
+
+    var email_input = document.createElement("input");
+    email_input.type = "hidden";
+    email_input.name = "customer_email";
     email_input.value = email_element.value;
+    form.appendChild(name_input);
+
+    if (!customers_pay_online) {
+        var payment_intent_id = document.getElementById("helper").getAttribute('data-payment_intent_id');
+        var payment_intent_id_input = document.createElement("input");
+        payment_intent_id_input.type = "hidden";
+        payment_intent_id_input.name = "payment_intent_id";
+        payment_intent_id_input.value = payment_intent_id;
+        form.appendChild(payment_intent_id_input);
+
+        var order_id = document.getElementById("helper").getAttribute('data-order_id');
+        var order_id_input = document.createElement("input");
+        order_id_input.type = "hidden";
+        order_id_input.name = "order_id";
+        order_id_input.value = order_id;
+        form.appendChild(order_id_input);
+        
+    } else {
+        var payment_intent_id = document.getElementById("helper").getAttribute('data-payment_intent_id');
+        var payment_intent_id_input = document.createElement("input");
+        payment_intent_id_input.type = "hidden";
+        payment_intent_id_input.name = "payment_intent_id";
+        payment_intent_id_input.value = payment_intent_id;
+        form.appendChild(payment_intent_id_input);
+
+        var connected_account_id = document.getElementById("helper").getAttribute('data-stripe_account_id');
+        var connected_account_id_input = document.createElement("input");
+        connected_account_id_input.type = "hidden";
+        connected_account_id_input.name = "connected_account";
+        connected_account_id_input.value = connected_account_id
+        form.appendChild(connected_account_id_input);
+    }
 
     var client_secret = document.getElementById("helper").getAttribute('data-stripe_client_secret');
-
-    var payment_intent_id = document.getElementById("helper").getAttribute('data-payment_intent_id');
-    var payment_intent_id_input = document.getElementById("payment_intent_id_inp");
-    payment_intent_id_input.value = payment_intent_id;
-
-    var connected_account_id = document.getElementById("helper").getAttribute('data-stripe_account_id');
-    var connected_account_id_input = document.getElementById("connected_account_id_inp");
-    connected_account_id_input.value = connected_account_id;
 
     // send post request with order_info form
     var xhr = new XMLHttpRequest();
@@ -135,53 +202,72 @@ function registerElements(elements, exampleName) {
         var border = document.getElementsByClassName("border")[0];
         var result_title = document.getElementsByClassName("result-title")[0];
 
-        if (event.target.response != "accepting orders") {
+        if (!customers_pay_online) {
+            
             // Stop loading!
             example.classList.remove('submitting');
             example.classList.add('submitted');
 
-            console.log(event.target.response);
-            checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
-            border.style.stroke = "#de0909";
-
-            if (event.target.response == "not accepting orders") {
+            if (event.target.response == "accepting orders") {
+                checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
+                result_title.innerHTML = "Thank you, your order has been placed.";
+                resetButton.style.display = "none";
+            } else if (event.target.response == "not accepting orders") {
                 result_title.innerHTML = "Sorry, orders are no longer being accepted.";
                 resetButton.style.display = "none";
-            } else if (event.target.response == "invalid email") {
-                result_title.innerHTML = "Payment failed: invalid email address.";
             } else {
                 result_title.innerHTML = "Payment failed";
             }
+
         } else {
-            var card = elements[0];
-            stripe.confirmCardPayment(client_secret, {
-                payment_method: {
-                    card: card,
-                }
-            }).then(function(result) {
+
+            if (event.target.response != "accepting orders") {
                 // Stop loading!
                 example.classList.remove('submitting');
                 example.classList.add('submitted');
 
-                if (result.error) {
-                    console.log(result.error.message);
-                    checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
-                    border.style.stroke = "#de0909";
+                console.log(event.target.response);
+                
+
+                if (event.target.response == "not accepting orders") {
+                    result_title.innerHTML = "Sorry, orders are no longer being accepted.";
+                    resetButton.style.display = "none";
+                } else if (event.target.response == "invalid email") {
+                    result_title.innerHTML = "Payment failed: invalid email address.";
+                } else {
                     result_title.innerHTML = "Payment failed";
                 }
-                else {
-                    // The payment has been processed!
-                    if (result.paymentIntent.status == 'succeeded') {
-                        console.log("success");
-                        checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
-                        var email_input = document.getElementById("email_inp");
-                        result_title.innerHTML = "Thank you, your order has been placed.";
-                        var caption = document.getElementById("caption");
-                        caption.innerHTML = "An email receipt has been sent to " + email_input.value + "."
-                        resetButton.style.display = "none";
+            } else {
+                var card = elements[0];
+                stripe.confirmCardPayment(client_secret, {
+                    payment_method: {
+                        card: card,
                     }
-                }
-            });
+                }).then(function(result) {
+                    // Stop loading!
+                    example.classList.remove('submitting');
+                    example.classList.add('submitted');
+
+                    if (result.error) {
+                        console.log(result.error.message);
+                        checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
+                        border.style.stroke = "#de0909";
+                        result_title.innerHTML = "Payment failed";
+                    }
+                    else {
+                        // The payment has been processed!
+                        if (result.paymentIntent.status == 'succeeded') {
+                            console.log("success");
+                            checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
+                            var email_input = document.getElementById("email_inp");
+                            result_title.innerHTML = "Thank you, your order has been placed.";
+                            var caption = document.getElementById("caption");
+                            caption.innerHTML = "An email receipt has been sent to " + email_input.value + "."
+                            resetButton.style.display = "none";
+                        }
+                    }
+                });
+            }
         }
     };
 
@@ -224,9 +310,4 @@ function registerElements(elements, exampleName) {
     example1.prepend(title);
   });
 });
-  var price = document.getElementById("helper").getAttribute('data-price');
-  price = price / 100;
-  price = price.toFixed(2);
-  var submit_button = document.getElementById("submit-button");
-  submit_button.innerHTML = "Pay $" + price;
 }
