77503db3b74f48888c84acc2a917ffde56cd243d
conradk16 <ckuklinsky21@amherst.edu>
2021-09-10 16:59:40 -0400

    finished tasks

--- embermill-aksum-website/webapp/static/js/home.js
+++ embermill-aksum-website/webapp/static/js/home.js
@@ -1,27 +1,28 @@
 var about_us_text = "Aksum is a city in northern Ethiopia. It's known for its tall, carved obelisks, relics of the ancient Kingdom of Aksum. Most are in the northern Stelae Park, including a huge fallen pillar, now in pieces. Centuries-old St. Mary of Zion is a Christian church and pilgrimage site believed to have housed the biblical Ark of the Covenant. The neighboring Chapel of the Tablet is said to contain the Ark today.";
 
 function add_news_element(_parent) {
-    var news_container = create_element("div", _parent, "desktop-home-news-container", null, null, null);
+    var news_container = create_element("div", _parent, "news-container", null, null, null);
     news_container.onclick = function () {
         window.location.href = 'https://www.independent.com/2021/06/14/ethiopian-food-coming-to-embermill/';
     }
 
     create_element("hr", news_container, null, null, null, null); 
 
-    var horizontal_holder = create_element("div", news_container, "desktop-home-horizontal-text-holder", null, null, null);
+    var horizontal_holder = create_element("div", news_container, "article-title-holder", null, null, null);
 
-    var paper_title = create_element("a", horizontal_holder, "desktop-home-paper-title", "Santa Barbara Independent:", null, null);
+    var article_title = create_element("a", horizontal_holder, "article-title", "Santa Barbara Independent: Ethiopian Food Coming to Embermill", null, null);
 
-    var article_title = create_element("a", horizontal_holder, "desktop-home-article-title", "Ethiopian Food Coming to Embermill", null, null);
-
-    var article_caption_holder = create_element("div", news_container, "desktop-home-article-caption-holder", null, null, null);
+    var article_caption_holder = create_element("div", news_container, "article-caption-holder", null, null, null);
     
-    var article_caption = create_element("a", article_caption_holder, "desktop-home-article-caption", "Saba Tewolde Hosting Tigray Benefit on June 20; Starts Serving Lunch at State Street Restaurant", null, null);
+    var article_caption = create_element("a", article_caption_holder, "article-caption", "Saba Tewolde Hosting Tigray Benefit on June 20; Starts Serving Lunch at State Street Restaurant", null, null);
 
     var gray_bar = document.createElement("hr");
     news_container.appendChild(gray_bar);
 }
 
+function set_column_img_width(img) {
+    img.width = (window.innerWidth - 20) / 3 - (10 * 2 / 3);
+}
 
 function load_mobile_view() {
     var welcome_side_bar_link = document.getElementsByClassName("side-menu-link")[0];
@@ -44,6 +45,25 @@ function load_mobile_view() {
     add_news_element(main_content);
 
     create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-2.jpeg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-4.jpg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-5.jpg");
+
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-6.jpg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-7.jpg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-8.jpg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-9.jpg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-10.jpg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-11.jpg");
+
+    create_element("img", main_content, "mobile-home-bottom-pic", null, null, "../static/media/restaurant-home-3.jpeg");
 }
 
 function load_desktop_view() {
@@ -72,4 +92,44 @@ function load_desktop_view() {
     bottom_pic.setAttribute('src', '../static/media/restaurant-home-2.jpeg');
     bottom_pic.classList.add("desktop-home-bottom-pic");
     main_content.appendChild(bottom_pic);
+
+    // create column pics
+    var column_pics_container = create_element("div", main_content, "desktop-home-column-pics-container", null, null, null);
+    
+    var column_1 = create_element("div", column_pics_container, "desktop-home-pics-column", null, null, null);
+
+    var column_2 = create_element("div", column_pics_container, "desktop-home-pics-column", null, null, null);
+
+    var column_3 = create_element("div", column_pics_container, "desktop-home-pics-column", null, null, null);
+
+    var img_1_1 = create_element("img", column_1, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-3.jpeg");
+    set_column_img_width(img_1_1);
+
+    var img_1_2 = create_element("img", column_1, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-4.jpg");
+    set_column_img_width(img_1_2);
+
+    var img_1_3 = create_element("img", column_1, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-5.jpg");
+    set_column_img_width(img_1_3);
+
+
+    var img_2_1 = create_element("img", column_2, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-6.jpg");
+    set_column_img_width(img_2_1);
+
+    var img_2_2 = create_element("img", column_2, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-7.jpg");
+    set_column_img_width(img_2_2);
+
+    var img_2_3 = create_element("img", column_2, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-8.jpg");
+    set_column_img_width(img_2_3);
+
+
+    var img_3_1 = create_element("img", column_3, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-9.jpg");
+    set_column_img_width(img_3_1);
+
+    var img_3_2 = create_element("img", column_3, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-10.jpg");
+    set_column_img_width(img_3_2);
+
+    var img_3_3 = create_element("img", column_3, "desktop-home-column-pic", null, null, "../static/media/restaurant-home-11.jpg");
+    set_column_img_width(img_3_3);
+
+    
 }
