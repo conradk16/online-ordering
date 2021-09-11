e3c80f55810fa12982f6391b7ffea8107a43cab9
conradk16 <ckuklinsky21@amherst.edu>
2021-09-11 15:07:18 -0400

    refactored index.js and cleared hidden form on reset

--- webapp/static/js/index.js
+++ webapp/static/js/index.js
@@ -15,71 +15,34 @@ if (customers_pay_online) {
 
 var restaurant_display_name = document.getElementById("helper").getAttribute('data-restaurant_display_name');
 
-function set_discount_error_msg() {
-    var msg = document.getElementById("error_msg");
-    msg.innerHTML = "*Invalid Discount Code";
+function set_error_msg(msg) {
+    var msg_elem = document.getElementById("error_msg");
+    msg_elem.innerHTML = msg;
 }
 
-function registerElements(elements, exampleName) {
-  var formClass = '.' + exampleName;
-  var example = document.querySelector(formClass);
-
-  var form = example.querySelector('form');
-  var resetButton = example.querySelector('a.reset');
-  var error = form.querySelector('.error');
-  var errorMessage = error.querySelector('.message');
-
-
-  if (!customers_pay_online) {
-    // change title
-    var title = document.getElementsByClassName("title")[0];
-    title.innerHTML = "Confirm Order"
-
-    // remove card input
-    var card_input = document.getElementById("example1-card");
-    var card_row = document.getElementById("card-row");
-    var card_fieldset = document.getElementById("card-fieldset");
-    card_input.remove();
-    card_row.remove();
-    card_fieldset.remove();
-
-    // set button text
-    var button = document.getElementById("submit-button");
-    button.innerHTML = "Place Pickup Order";
-  } else {
-    // set button text
-    var price = document.getElementById("helper").getAttribute('data-price');
-    price = price / 100;
-    price = price.toFixed(2);
-    var submit_button = document.getElementById("submit-button");
-    submit_button.innerHTML = "Pay $" + price;
-  }
-
-
-
-  function enableInputs() {
+function enableInputs(form) {
     Array.prototype.forEach.call(
-      form.querySelectorAll(
-        "input[type='text'], input[type='email'], input[type='tel']"
-      ),
-      function(input) {
-        input.removeAttribute('disabled');
-      }
+        form.querySelectorAll(
+            "input[type='text'], input[type='email'], input[type='tel']"
+        ),
+        function(input) {
+            input.removeAttribute('disabled');
+        }
     );
-  }
+}
 
-  function disableInputs() {
+function disableInputs(form) {
     Array.prototype.forEach.call(
-      form.querySelectorAll(
-        "input[type='text'], input[type='email'], input[type='tel']"
-      ),
-      function(input) {
-        input.setAttribute('disabled', 'true');
-      }
+        form.querySelectorAll(
+            "input[type='text'], input[type='email'], input[type='tel']"
+        ),
+        function(input) {
+            input.setAttribute('disabled', 'true');
+        }
     );
-  }
+}
 
-  function triggerBrowserValidation() {
+function triggerBrowserValidation(form) {
     // The only way to trigger HTML5 form validation UI is to fake a user submit
     // event.
     var submit = document.createElement('input');
@@ -88,39 +51,151 @@ function registerElements(elements, exampleName) {
     form.appendChild(submit);
     submit.click();
     submit.remove();
-  }
+}
 
-  // Listen for errors from each Element, and show error messages in the UI.
-  var savedErrors = {};
-  elements.forEach(function(element, idx) {
-    element.on('change', function(event) {
-      if (event.error) {
-        error.classList.add('visible');
-        savedErrors[idx] = event.error.message;
-        errorMessage.innerText = event.error.message;
-      } else {
-        savedErrors[idx] = null;
+function xhrOnLoadPayOnline(event, example, resetButton, client_secret, card, email_input_value) {
+    var checkmark = document.getElementsByClassName("checkmark")[0];
+    var border = document.getElementsByClassName("border")[0];
+    var result_title = document.getElementsByClassName("result-title")[0];
 
-        // Loop over the saved errors and find the first one, if any.
-        var nextError = Object.keys(savedErrors)
-          .sort()
-          .reduce(function(maybeFoundError, key) {
-            return maybeFoundError || savedErrors[key];
-          }, null);
+    if (event.target.response != "accepting orders") {
+        // Stop loading!
+        example.classList.remove('submitting');
+        example.classList.add('submitted');
 
-        if (nextError) {
-          // Now that they've fixed the current error, show another one.
-          errorMessage.innerText = nextError;
+        console.log(event.target.response);
+
+
+        if (event.target.response == "not accepting orders") {
+            result_title.innerHTML = "Sorry, orders are no longer being accepted.";
+            resetButton.style.display = "none";
+        } else if (event.target.response == "invalid email") {
+            result_title.innerHTML = "Payment failed: invalid email address.";
         } else {
-          // The user fixed the last error; no more errors.
-          error.classList.remove('visible');
+            result_title.innerHTML = "Payment failed";
         }
-      }
-    });
-  });
+    } else {
+        stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: card,
+            }
+        }).then(function(result) {
+            // Stop loading!
+            example.classList.remove('submitting');
+            example.classList.add('submitted');
 
-  // Listen on the form's 'submit' handler...
-  form.addEventListener('submit', function(e) {
+            if (result.error) {
+                console.log(result.error.message);
+                checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
+                border.style.stroke = "#de0909";
+                result_title.innerHTML = "Payment failed";
+            }
+            else {
+                // The payment has been processed!
+                if (result.paymentIntent.status == 'succeeded') {
+                    console.log("success");
+                    checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
+                    result_title.innerHTML = "Thank you, your order has been placed.";
+                    var caption = document.getElementById("caption");
+                    caption.innerHTML = "An email receipt has been sent to " + email_input_value + "."
+                    resetButton.style.display = "none";
+                }
+            }
+        });
+    }
+}
+
+function xhrOnLoadPayInStore(event, example, resetButton) {
+    console.log(event.target.response);
+    var checkmark = document.getElementsByClassName("checkmark")[0];
+    var border = document.getElementsByClassName("border")[0];
+    var result_title = document.getElementsByClassName("result-title")[0];
+
+    // Stop loading!
+    example.classList.remove('submitting');
+    example.classList.add('submitted');
+
+    if (event.target.response == "accepting orders") {
+        checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
+        result_title.innerHTML = "Thank you, your order has been placed.";
+        resetButton.style.display = "none";
+    } else if (event.target.response == "not accepting orders") {
+        checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
+        border.style.stroke = "#de0909";
+        result_title.innerHTML = "Sorry, orders are no longer being accepted.";
+        resetButton.style.display = "none";
+    } else if (event.target.response == "discount applied") {
+        checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
+        result_title.innerHTML = "Thank you, your order has been placed.";
+        var caption = document.getElementById("caption");
+        caption.innerHTML = "Your discount will be applied when you pay in-store."
+        resetButton.style.display = "none";
+    } else if (event.target.response == "discount failed") {
+        example.classList.remove('submitted');
+        // Show title
+        var title = document.getElementsByClassName("title")[0];
+        title.style.display = "block";
+        var form = document.getElementById("form");
+        resetForm(form, "*Invalid Discount Code", example, resetButton);
+    } else {
+        checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
+        border.style.stroke = "#de0909";
+        result_title.innerHTML = "Order failed.";
+    }
+}
+
+function resetFormFromClick(e, form, elements, error, error_msg, example, resetButton) {
+    e.preventDefault();
+
+    // Clear each Element.
+    elements.forEach(function(element) {
+        element.clear();
+    });
+
+    // Reset error state as well.
+    error.classList.remove('visible');
+
+    resetForm(form, error_msg, example, resetButton);
+}
+
+function resetForm(form, error_msg, example, resetButton) {
+    // Resetting the form (instead of setting the value to `''` for each input)
+    // helps us clear webkit autofill styles.
+    form.reset();
+
+    // reset border
+    var border = document.getElementsByClassName("border")[0];
+    border.style.stroke = "#87bbfd";
+
+
+    // Resetting the form does not un-disable inputs, so we need to do it separately:
+    enableInputs(form);
+    example.classList.remove('submitted');
+
+    // remove title if necessary
+    var title = document.getElementsByClassName("title")[0];
+    if (title) { title.remove(); }
+
+    // add back the title
+    var example1 = document.getElementsByClassName("cell example example1")[0];
+    var title = document.createElement("h1");
+    title.classList.add("title");
+    title.innerHTML = "Enter Payment Details";
+    example1.prepend(title);
+
+    // set the error message
+    set_error_msg(error_msg);
+
+    // clear the (hidden) order info form
+    var order_info_form = document.getElementById("order_info");
+    const num_children = order_info_form.length;
+    for (let i = 0; i < num_children; i++) {
+        order_info_form[0].remove();
+    }
+
+}
+
+function onSubmit(e, form, example, resetButton) {
     e.preventDefault();
 
     // Trigger HTML5 validation UI on the form if any of the inputs fail
@@ -136,7 +211,7 @@ function registerElements(elements, exampleName) {
           }
         });
         if (!plainInputsValid) {
-          triggerBrowserValidation();
+          triggerBrowserValidation(form);
           return;
         }
     }
@@ -149,10 +224,9 @@ function registerElements(elements, exampleName) {
     example.classList.add('submitting');
 
     // Disable all inputs.
-    disableInputs();
+    disableInputs(form);
 
     // set order_info form data
-
     var name_element = document.getElementById("example1-name");
     var email_element = document.getElementById("example1-email");
     var discount_code_element = document.getElementById("example1-discount-code");
@@ -206,140 +280,87 @@ function registerElements(elements, exampleName) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/update-order-details");
     xhr.onload = function(event) {
-
-        var checkmark = document.getElementsByClassName("checkmark")[0];
-        var border = document.getElementsByClassName("border")[0];
-        var result_title = document.getElementsByClassName("result-title")[0];
-
         if (!customers_pay_online) {
-
-            // Stop loading!
-            example.classList.remove('submitting');
-            example.classList.add('submitted');
-
-            if (event.target.response == "accepting orders") {
-                checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
-                result_title.innerHTML = "Thank you, your order has been placed.";
-                resetButton.style.display = "none";
-            } else if (event.target.response == "not accepting orders") {
-                checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
-                border.style.stroke = "#de0909";
-                result_title.innerHTML = "Sorry, orders are no longer being accepted.";
-                resetButton.style.display = "none";
-            } else if (event.target.response == "discount applied") {
-                checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
-                result_title.innerHTML = "Thank you, your order has been placed.";
-                var caption = document.getElementById("caption");
-                caption.innerHTML = "Your discount will be applied when you pay in-store."
-                resetButton.style.display = "none";
-            } else if (event.target.response == "discount failed") {
-                example.classList.remove('submitted');
-                // Show title
-                var title = document.getElementsByClassName("title")[0];
-                title.style.display = "block";
-                set_discount_error_msg();
-                var form = document.getElementById("form");
-                form.reset();
-                var name_inp = document.getElementById("example1-name");
-                var email_inp = document.getElementById("example1-email");
-                var discount_inp = document.getElementById("example1-discount-code");
-                name_inp.disabled = false;
-                email_inp.disabled = false;
-                discount_inp.disabled = false;
-            } else {
-                checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
-                border.style.stroke = "#de0909";
-                result_title.innerHTML = "Order failed.";
-            }
-
+            xhrOnLoadPayInStore(event, example, resetButton);
         } else {
-
-            if (event.target.response != "accepting orders") {
-                // Stop loading!
-                example.classList.remove('submitting');
-                example.classList.add('submitted');
-
-                console.log(event.target.response);
-
-
-                if (event.target.response == "not accepting orders") {
-                    result_title.innerHTML = "Sorry, orders are no longer being accepted.";
-                    resetButton.style.display = "none";
-                } else if (event.target.response == "invalid email") {
-                    result_title.innerHTML = "Payment failed: invalid email address.";
-                } else {
-                    result_title.innerHTML = "Payment failed";
-                }
-            } else {
-                var card = elements[0];
-                stripe.confirmCardPayment(client_secret, {
-                    payment_method: {
-                        card: card,
-                    }
-                }).then(function(result) {
-                    // Stop loading!
-                    example.classList.remove('submitting');
-                    example.classList.add('submitted');
-
-                    if (result.error) {
-                        console.log(result.error.message);
-                        checkmark.setAttribute("d", "M25 25 59 59 M 25 59 59 25");
-                        border.style.stroke = "#de0909";
-                        result_title.innerHTML = "Payment failed";
-                    }
-                    else {
-                        // The payment has been processed!
-                        if (result.paymentIntent.status == 'succeeded') {
-                            console.log("success");
-                            checkmark.setAttribute("d", "M23.375 42.5488281 36.8840688 56.0578969 64.891932 28.0500338");
-                            result_title.innerHTML = "Thank you, your order has been placed.";
-                            var caption = document.getElementById("caption");
-                            caption.innerHTML = "An email receipt has been sent to " + email_input.value + "."
-                            resetButton.style.display = "none";
-                        }
-                    }
-                });
-            }
+            xhrOnLoadPayOnline(event, example, resetButton, client_secret, elements[0], email_input.value);
         }
-    };
+    }
 
     var order_info_formData = new FormData(document.getElementById("order_info"));
     xhr.send(order_info_formData);
 
-
-  resetButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    // Resetting the form (instead of setting the value to `''` for each input)
-    // helps us clear webkit autofill styles.
-    form.reset();
-
-    // Clear each Element.
-    elements.forEach(function(element) {
-      element.clear();
+    resetButton.addEventListener('click', function(e) {
+        resetFormFromClick(e, form, elements, error, "", example, resetButton);
     });
+}
 
-    // Reset error state as well.
-    error.classList.remove('visible');
+function registerElements(elements, exampleName) {
+  var formClass = '.' + exampleName;
+  var example = document.querySelector(formClass);
 
-    // reset border
-    var border = document.getElementsByClassName("border")[0];
-    border.style.stroke = "#87bbfd";
+  var form = example.querySelector('form');
+  var resetButton = example.querySelector('a.reset');
+  var error = form.querySelector('.error');
+  var errorMessage = error.querySelector('.message');
 
 
-    // Resetting the form does not un-disable inputs, so we need to do it separately:
-    enableInputs();
-    example.classList.remove('submitted');
-
-    // remove title if necessary
+  if (!customers_pay_online) {
+    // change title
     var title = document.getElementsByClassName("title")[0];
-    if (title) { title.remove(); }
+    title.innerHTML = "Confirm Order"
 
-    // add back the title
-    var example1 = document.getElementsByClassName("cell example example1")[0];
-    var title = document.createElement("h1");
-    title.classList.add("title");
-    title.innerHTML = "Enter Payment Details";
-    example1.prepend(title);
+    // remove card input
+    var card_input = document.getElementById("example1-card");
+    var card_row = document.getElementById("card-row");
+    var card_fieldset = document.getElementById("card-fieldset");
+    card_input.remove();
+    card_row.remove();
+    card_fieldset.remove();
+
+    // set button text
+    var button = document.getElementById("submit-button");
+    button.innerHTML = "Place Pickup Order";
+  } else {
+    // set button text
+    var price = document.getElementById("helper").getAttribute('data-price');
+    price = price / 100;
+    price = price.toFixed(2);
+    var submit_button = document.getElementById("submit-button");
+    submit_button.innerHTML = "Pay $" + price;
+  }
+
+  // Listen for errors from each Element, and show error messages in the UI.
+  var savedErrors = {};
+  elements.forEach(function(element, idx) {
+    element.on('change', function(event) {
+      if (event.error) {
+        error.classList.add('visible');
+        savedErrors[idx] = event.error.message;
+        errorMessage.innerText = event.error.message;
+      } else {
+        savedErrors[idx] = null;
+
+        // Loop over the saved errors and find the first one, if any.
+        var nextError = Object.keys(savedErrors)
+          .sort()
+          .reduce(function(maybeFoundError, key) {
+            return maybeFoundError || savedErrors[key];
+          }, null);
+
+        if (nextError) {
+          // Now that they've fixed the current error, show another one.
+          errorMessage.innerText = nextError;
+        } else {
+          // The user fixed the last error; no more errors.
+          error.classList.remove('visible');
+        }
+      }
+    });
   });
-});
+ 
+  // Listen on the form's 'submit' handler...
+  form.addEventListener('submit', function(e) {
+    onSubmit(e, form, example, resetButton);
+  });
 }
